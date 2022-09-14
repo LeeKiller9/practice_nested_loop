af37f14386782b1b67569be6492b961b1003301d
LeeKiller9 <112848883+LeeKiller9@users.noreply.github.com>
2022-09-14 16:00:17 +0700

    update feauture

--- javascript.js
+++ javascript.js
@@ -1,14 +1,32 @@
-let table = document.getElementById('myTable')
+let tableFor = document.getElementById('myTableFor')
+let tableWhile = document.getElementById('myTableWhile')
 let i
 let j
 
-function showTable() {
+function showTableFor() {
     for (i = 10; i >= 1; i--) {
-        let row = table.insertRow(0)
+        let row = tableFor.insertRow(0);
         for (j = 0; j <= 9; j++) {
-            let cell = row.insertCell(j)
-            let dataCell = i * (j + 1)
+            let cell = row.insertCell(j);
+            let dataCell = i * (j + 1);
             cell.innerHTML = '' + dataCell;
         }
     }
+    document.getElementById('nestFor').disabled = 'disabled';
+}
+
+function showTableWhile() {
+    let i = 10;
+    while (i >= 1) {
+        let row = tableWhile.insertRow(0)
+        let j = 0;
+        while (j <= 9) {
+            let cell = row.insertCell(j);
+            let dataCell = i * (j + 1);
+            cell.innerHTML = '' + dataCell;
+            j++
+        }
+        i--
+    }
+    document.getElementById('nestWhile').disabled = 'disabled';
 }
